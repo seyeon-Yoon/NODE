139ba29e5815aecdff7997b601ba7127c02a59ef
seyeon-Yoon <fjrkdlxm100@gmail.com>
2025-01-12 02:38:20 +0900

    UserStorage모델 구현

---
+++ app/src/models/UserStorage.js
@@ -0,0 +1,22 @@
+"use strict";
+
+class UserStorage {
+    static #users = {
+        id: ["태그"],
+        password: ["1234"],
+        name: ["윤세연"],
+    };
+
+    static getUsers(...fields) {
+        const users = this.#users;
+        const newUsers = fields.reduce((newUsers, field) => {
+            if (users.hasOwnProperty(field)) {
+                newUsers[field] = users[field];
+            }
+            return newUsers;
+        }, {});
+        return newUsers;
+    }
+}
+
+module.exports = UserStorage;
