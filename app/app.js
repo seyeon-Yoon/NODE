09e5d4487dd24a2a3d95a11e489851f45e60afc5
seyeon-Yoon <fjrkdlxm100@gmail.com>
2025-01-11 23:42:34 +0900

    폴더구조 최적화

--- app/app.js
+++ app/app.js
@@ -5,11 +5,11 @@ const express = require('express') //express import
 const app = express() //express import
 
 //라우팅
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 
 
 //앱 세팅
-app.set("views", "./views"); //뷰엔진에 대한 세팅
+app.set("views", "./src/views"); //뷰엔진에 대한 세팅
 app.set("view engine", "ejs"); //뷰엔진을 ejs로 쓰겠다
 
 app.use("/", home); // use -> 미들웨어를 등록해주는 메서드
