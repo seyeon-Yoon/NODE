abcbea694e8dedd5b770262cf1b7723b59cf9f31
seyeon-Yoon <fjrkdlxm100@gmail.com>
2025-01-12 02:10:07 +0900

    프론트 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -22,5 +22,14 @@ function login() {
         body: JSON.stringify(req),  // 해당데이터를 문자열로 바꿔주는 메서드
     })
         .then((res) => res.json())
-        .then((res) => console.log(res));
+        .then((res) => {
+            if (res.success) {
+                location.href = '/';
+            } else {
+                alert(res.msg);
+            }
+        })
+        .catch((err) => {
+            console.error("로그인 중 에러 발생");
+        });
 }
