d40b55114cdf0e7f01ab1fe34dcafb294732c888
seyeon-Yoon <fjrkdlxm100@gmail.com>
2025-01-12 00:02:09 +0900

    프론트 단 javascrpit파일 만들기

--- app/app.js
+++ app/app.js
@@ -12,6 +12,7 @@ const home = require("./src/routes/home");
 app.set("views", "./src/views"); //뷰엔진에 대한 세팅
 app.set("view engine", "ejs"); //뷰엔진을 ejs로 쓰겠다
 
+app.use(express.static(`${__dirname}/src/public`)); //__dirname은 현재 app.js의 위치를 반환 이 안에 /src/public을 정적경로로 추가하겠다는 뜻
 app.use("/", home); // use -> 미들웨어를 등록해주는 메서드
 
 module.exports = app;
