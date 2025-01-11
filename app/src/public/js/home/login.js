4d81c96538b4f06f2034041b4e5a8c8fda322cd0
seyeon-Yoon <fjrkdlxm100@gmail.com>
2025-01-12 00:30:43 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,23 @@
 "use strict";
 
-console.log("hello");
-console.log("bye");
+const id = document.querySelector("#id"),
+    password = document.querySelector("#password"),
+    loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        password : password.value,
+    };
+    console.log(req);
+    console.log(JSON.stringify(req));
+    fetch("/login", {
+        method: "POST", //restAPI와 관련되어있음.
+        headers: {  //데이터타입을 명시적으로 알려줌
+            "Content-Type" : "application/json" //내가보내는 데이터타입
+        },
+        body: JSON.stringify(req)  // 해당데이터를 문자열로 바꿔주는 메서드
+    })
+}
