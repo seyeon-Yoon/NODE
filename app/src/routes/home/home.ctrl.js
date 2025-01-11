37f393928756d72a70f2d2922b31480b3122c1cf
seyeon-Yoon <fjrkdlxm100@gmail.com>
2025-01-12 03:17:56 +0900

    User 모델 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,6 +1,8 @@
 "use strict";
 
-const UserStorage = require("../../models/UserStorage");
+const { response } = require("express");
+const User = require("../../models/User");
+
 const output = {
     home: (req, res) => {
         res.render('home/index'); //   /index.ejs를 불러와라
@@ -12,22 +14,8 @@ const output = {
 
 const process = {
     login : (req, res) => {
-        const id = req.body.id,
-            password = req.body.password;
-
-        const users = UserStorage.getUsers("id", "password");
-
-        const response ={};
-        if (users.id.includes(id)) {
-            const idx = users.id.indexOf(id);
-            if (users.password[idx] === password) {
-                response.success = true;
-                return res.json(response);
-            }
-        }
-
-        response.success = false;
-        response.msg = "로그인에 실패하셨습니다.";
+        const user = new User(req.body);
+        const response = user.login();
         return res.json(response);
     },   
 };
@@ -36,3 +24,4 @@ module.exports = {  //모듈은 key : value 형태인데 아래와 같이 key만
     output,
     process,
 };
+
